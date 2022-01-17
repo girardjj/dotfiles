1288e36f2ac6760997e42b33967d1c1f3138425d
Girard <girardjj@acct02.us.lmco.com>
2022-01-17 16:11:21 -0700

    install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ln -sf $HOME/dotfiles/.bash_profile $HOME/.bash_profile
 ln -sf $HOME/dotfiles/.bashrc $HOME/.bashrc
-ln -sf $HOME/dotfiles/.aliases $HOME/.aliases
+ln -sf $HOME/dotfiles/.bash_aliases $HOME/.bash_aliases
 
